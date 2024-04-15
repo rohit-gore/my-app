dfbcb19d8f7d0f338b0384b27686326c1dc87843
rohit-gore <679.nine@gmail.com>
2024-04-16 00:26:09 +0530

    notes update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import {useEffect} from 'react';
 // 2e86ea9f
 const API_URL = "https://www.omdbapi.com?apikey=2e86ea9f";
+
 const App = () => {
+
+    const searchMovies = async (title) => {
+        
+    }
+
+    useEffect(() => {
+
+    }, []); 
+    // dependecy array '[]' is there if we only want to call useEffect at the start.
+    // async -> asyncronus data -> means it takes some time to featch the data.
+
     return(
         <h1>App</h1>
     );
