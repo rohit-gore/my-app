15d4fbcbb3e26259b56123021924a76725268c14
rohit-gore <679.nine@gmail.com>
2024-04-15 21:47:48 +0530

    api first time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import {useEffect} from 'react';
+// 2e86ea9f
+const API_URL = "https://www.omdbapi.com?apikey=2e86ea9f";
 const App = () => {
     return(
         <h1>App</h1>
