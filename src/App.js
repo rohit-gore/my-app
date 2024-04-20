ca2de5a18eba2f175f7c3011f166ac1654e9c43a
rohit-gore <679.nine@gmail.com>
2024-04-20 22:23:43 +0530

    css code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import {useEffect} from 'react';
 // 2e86ea9f
-const API_URL = "https://www.omdbapi.com?apikey=2e86ea9f";
+const API_URL = 'https://www.omdbapi.com?apikey=2e86ea9f';
 
 const App = () => {
 
     const searchMovies = async (title) => {
-        
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
     }
 
     useEffect(() => {
-
+        searchMovies('The Grand Tour');
     }, []); 
     // dependecy array '[]' is there if we only want to call useEffect at the start.
     // async -> asyncronus data -> means it takes some time to featch the data.
